44e50a669bf109bb1415057138eaa082dc9878f3
plosslaw <plosslaw@gmail.com>
2022-12-21 19:21:21 +0800

    Updates

---
+++ precache-manifest.6747c4befb4ddcff93c266a40cf45167.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "526a05b9999613c4bf4d89c31b151c3a",
+    "url": "/magicians-red/index.html"
+  },
+  {
+    "revision": "36a04445c63fd00b782d",
+    "url": "/magicians-red/static/css/main.0ea783dd.chunk.css"
+  },
+  {
+    "revision": "37d369b4d596bb637d2b",
+    "url": "/magicians-red/static/js/2.084cd401.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/magicians-red/static/js/2.084cd401.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36a04445c63fd00b782d",
+    "url": "/magicians-red/static/js/main.f16746b0.chunk.js"
+  },
+  {
+    "revision": "9f2d4ca46914598ec9ab",
+    "url": "/magicians-red/static/js/runtime-main.e39da750.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/magicians-red/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/magicians-red/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/magicians-red/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/magicians-red/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/magicians-red/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
